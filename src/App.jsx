05998cd7f4ec8cea665da0a1c2e3c2b13f92498e
Fahad <fahadbinqaiser3@gmail.com>
2025-08-22 10:39:08 +0500

    Setting up fonts

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,13 @@
 import { useState } from "react";
-
+import React from "react";
 function App() {
-  return <></>;
+  return (
+    <>
+      <h1 className="text-7xl text-balance font-bold text-white z-10">
+        Turn Visitors into Customers.
+      </h1>
+    </>
+  );
 }
 
 export default App;
