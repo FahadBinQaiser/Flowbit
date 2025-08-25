2686a284856299c53361298948790598de459c7f
Fahad <fahadbinqaiser3@gmail.com>
2025-08-25 16:16:37 +0500

    Fixed navbar

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -10,13 +10,17 @@ const Navbar = () => {
         <div className="text-3xl text-white">Flowbit</div>
 
         <div
-          className="hidden md:flex justify-end items-center bg-transparent backdrop-blur-3xl px-8 py-3 rounded-xl space-x-8"
+          className="hidden md:flex justify-end items-center bg-transparent backdrop-blur-3xl pl-8 pr-3 py-3 rounded-xl space-x-8"
           style={{
             boxShadow: "3px 0 12px rgba(250, 250, 250, 0.14)",
           }}
         >
           {["Home", "Features", "Pricing", "About"].map((link) => (
-            <a key={link} href="#" className="relative text-white group">
+            <a
+              key={link}
+              href="#"
+              className="relative body_font text-white group"
+            >
               {link}
               <span className="absolute left-0 -bottom-1 w-0 h-[2px] bg-[#C084FC] transition-all duration-300 group-hover:w-full"></span>
             </a>
@@ -34,7 +38,7 @@ const Navbar = () => {
       </div>
 
       <div
-        className={`md:hidden fixed top-20 right-0 w-64 bg-[#fafafa] backdrop-blur-3xl shadow-xl rounded-l-2xl transform transition-transform duration-300 ease-in-out z-50
+        className={`md:hidden fixed top-20 right-0 w-64 bg-transparent backdrop-blur-3xl shadow-xl rounded-l-2xl transform transition-transform duration-300 ease-in-out z-50
       ${open ? "translate-x-0" : "translate-x-full"}`}
       >
         <div className="flex flex-col items-center px-6 py-10 space-y-6">
@@ -42,7 +46,7 @@ const Navbar = () => {
             <a
               key={link}
               href="#"
-              className="text-lg font-medium text-[#C084FC] hover:text-[#7C3AED] transition"
+              className="text-lg font-medium text-[#fafafa] transition"
             >
               {link}
             </a>
