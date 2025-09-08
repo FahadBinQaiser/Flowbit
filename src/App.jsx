5d6d8f094c2fabf86e731e01fa9d9bd087965bd0
Fahad <fahadbinqaiser3@gmail.com>
2025-09-08 13:34:44 +0500

    Fixing imports

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,7 @@ import { useState } from "react";
 import React from "react";
 import Navbar from "./components/Navbar/Navbar.jsx";
 import HeroSection from "./components/HeroSection/HeroSection.jsx";
-import AboutSection from "./components/AboutSection.jsx";
+import AboutSection from "./components/AboutSection/AboutSection.jsx";
 function App() {
   return (
     <>
