c123a01c508b840128291b2c5f23db2c138b51c8
Fahad <fahadbinqaiser3@gmail.com>
2025-09-09 19:16:47 +0500

    Problem section started!

--- src/App.jsx
+++ src/App.jsx
@@ -3,12 +3,14 @@ import React from "react";
 import Navbar from "./components/Navbar/Navbar.jsx";
 import HeroSection from "./components/HeroSection/HeroSection.jsx";
 import AboutSection from "./components/AboutSection/AboutSection.jsx";
+import ProblemSection from "./components/ProblemSection/ProblemSection.jsx";
 function App() {
   return (
     <>
       <Navbar />
       <HeroSection />
       <AboutSection />
+      <ProblemSection />
     </>
   );
 }
